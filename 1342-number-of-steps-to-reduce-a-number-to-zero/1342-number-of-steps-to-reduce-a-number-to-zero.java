ec8061cabdcbf3713dc4243986084771b440d8e1
jiyeon <69364279+yeonjiyeon@users.noreply.github.com>
2022-05-14 22:58:28 +0900

    Time: 1 ms (53.54%), Space: 41.2 MB (36.44%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int answer = 0;
+        while(num > 0){
+            if(num % 2 == 0){
+              num /= 2;
+              answer++;
+            } 
+            else{
+              num -= 1;
+              answer++;
+            } 
+        }
+        
+        return answer;
+    }
+}
